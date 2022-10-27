c9574dc7085097ecbe2ec7c12ba1924053bb9750
PacoCP5 <pacocamaraparra5@gmail.com>
2022-10-27 13:56:45 +0200

    :construction: closes #13 y avanzo #14 #15 y #16 con la implementación de los conceptos en el lenguaje elegido

---
+++ src/tramo.ts
@@ -0,0 +1,43 @@
+import {Parada} from "./parada"
+
+export class Tramo {
+
+    private origenTramo: Parada;
+    private destinoTramo: Parada;
+    private precioTramo: number;
+    private tiempoTramo: number;
+
+    constructor(origenTramo: Parada, destinoTramo: Parada, precioTramo: number, tiempoTramo: number) {
+        this.origenTramo = origenTramo;
+        this.destinoTramo = destinoTramo;
+
+        if(precioTramo >= 0) {
+          this.precioTramo = precioTramo;
+        } else {
+          throw new Error("El precio del tramo no puede ser negativo");
+        }
+
+        if(tiempoTramo > 0) {
+          this.tiempoTramo = tiempoTramo;
+        } else {
+          throw new Error("El tiempo del tramo no puede ser negativo");
+        }
+    }
+
+    public getOrigenTramo(): string {
+      return this.origenTramo;
+    }
+
+    public getDestinoTramo(): string {
+      return this.destinoTramo;
+    }
+
+    public getPrecioTramo(): number {
+      return this.precioTramo;
+    }
+
+    public getTiempoTramo(): number {
+      return this.tiempoTramo;
+    }
+
+}
