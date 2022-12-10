ff333d35693f32cd37868bf5390740e96ff5822d
MarinoFajardo <marinofajardo@correo.ugr.es>
2022-12-10 18:59:32 +0100

    Arreglo error del formato del tiempo en el test de Tramo Se ha corregido el valor del tiempo expresándolo en minutos

--- src/test/tramo.test.ts
+++ src/test/tramo.test.ts
@@ -4,7 +4,7 @@ import {isOriginEqualToDestiny} from "../tramo"
 
 const origen = new Parada("001","Granada");
 const destino = new Parada("002","Almuñécar");
-const tramo = new Tramo(origen,destino,1.4,5.2);
+const tramo = new Tramo(origen,destino,1.4,30);
 
 test("Origen y Destino de Tramo no pueden ser iguales", () => {
   expect(isOriginEqualToDestiny(tramo)).toBe(false);
