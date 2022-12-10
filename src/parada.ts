f05dddfb6ae254a7bcb70c95ef8f244e3722ee4a
MarinoFajardo <marinofajardo@correo.ugr.es>
2022-12-10 18:45:47 +0100

    Avanza #25 y closes #28 #31 con los test de la clase Parada Se han creeado los test para comprobar la lógica de negocio en la clase Parada

--- src/parada.ts
+++ src/parada.ts
@@ -7,4 +7,20 @@ export class Parada{
         this.idParada=idParada;
         this.ciudad = ciudad;
     }
+    
+    public getidParada():string{
+        return this.idParada;
+    }
+
+    public getCiudad():string{
+        return this.ciudad;
+    }
+}
+
+export function areParadaIdEqual(a:Parada, b:Parada):boolean{
+    if((a.getCiudad() != b.getCiudad())&&(a.getidParada() == b.getidParada())){
+        return true;
+    }else{
+        return false;
+    }
 }
