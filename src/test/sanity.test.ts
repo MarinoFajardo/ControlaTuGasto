c40e9255fc0adb2d87963545872cc12926832edc
MarinoFajardo <marinofajardo@correo.ugr.es>
2022-12-15 10:07:17 +0100

    Actualización del fichero de datos de prueba Se ha acualizado el fichero de datos de prueba añadiendo nuevos valores por lo que se ha modificado la constante del test

--- src/test/sanity.test.ts
+++ src/test/sanity.test.ts
@@ -1,6 +1,6 @@
 import tramos from "../data/datos.json"
 
-const totalTramos = 9;
+const totalTramos = 12;
 
 test("Se han leido el número correcto de Tramos", () => {
   expect(tramos.length).toBe(totalTramos);
