96b6176d6d5468555646d91a56fc4a9ab8e86fa3
MarinoFajardo <marinofajardo@correo.ugr.es>
2022-12-16 09:05:08 +0100

    Actualiza #26 con una nueva implementación de los test Se han eliminado funciones innecesarias en la clase tramo y se han actualizado los test

--- src/test/tramo.test.ts
+++ src/test/tramo.test.ts
@@ -1,19 +1,18 @@
 import { Parada } from "../parada"
-import {isPrizePositive, isTimePositive, Tramo} from "../tramo"
-import {isOriginEqualToDestiny} from "../tramo"
+import {Tramo} from "../tramo"
 
 const origen = new Parada("001","Granada");
 const destino = new Parada("002","Almuñécar");
 const tramo = new Tramo(origen,destino,1.4,30);
 
 test("Origen y Destino de Tramo no pueden ser iguales", () => {
-  expect(isOriginEqualToDestiny(tramo)).toBe(false);
+  expect(tramo.getOrigenTramo() == tramo.getDestinoTramo()).toBe(false);
 });
 
 test("El precio de un Tramo no puede ser negativo",() => {
-  expect(isPrizePositive(tramo)).toBe(true);
+  expect(tramo.getPrecioTramo()).toBeGreaterThan(0);
 });
 
 test("La duración de un Tramo no puede ser negativa",() => {
-  expect(isTimePositive(tramo)).toBe(true);
+  expect(tramo.getTiempoTramo()).toBeGreaterThan(0);
 });
