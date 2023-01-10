b89db36069dc6943ba4bc9a1061e2880d7e3ec95
MarinoFajardo <marinofajardo@correo.ugr.es>
2023-01-10 13:13:31 +0100

    closes #37 #39 con la representación de árbol Se ha creado la clase árbol con la que se va a representar la estructura de datos

--- src/arbol.ts
+++ src/arbol.ts
@@ -9,5 +9,81 @@ export interface Nodo{
   hermanoDrch: Nodo | null;
 }
 
+export class Arbol{
+  private root:Nodo;
+  private peso:number;
 
+  constructor(root:Nodo){
+    this.root = root;
+    this.peso = 1; //Representa el número de nodos del árbol. Cuando se crea un árbol, el peso inicial el 1 ya que solo tiene el nodo raíz
+  }
+  
+  //Usado en el algoritmo
+  public getRoot():Nodo{
+    return this.root;
+  }
+
+  //Usado en el algoritmo
+  public getPeso():number{
+    return this.peso;
+  }
+
+  //Cada vez que añadimos un nodo en el árbol incrementamos su peso.
+  public incrementarPeso(){
+    this.peso++;
+  }
+
+  // Función que genera el árbol con el nodo root como origen de la ruta y el destino como las hojas del
+  public generarArbol(tramos:Tramo[],destino:string){
+    let nodoActual:Nodo = this.root;
+    for(let tramo of tramos){
+      if(tramo.getOrigenTramo().getCiudad() == nodoActual.parada?.getCiudad()){
+        if(nodoActual.hijoIzq == null){
+          let hijo : Nodo ={
+            parada : new Parada(tramo.getDestinoTramo().getidParada(), tramo.getDestinoTramo().getCiudad()),
+            coste : Number(tramo.getPrecioTramo()),
+            padre : nodoActual,
+            hijoIzq : null,
+            hermanoDrch : null
+          };
+          nodoActual.hijoIzq = hijo;
+          nodoActual = hijo;
+          this.incrementarPeso();
+        }else{
+          let hermano : Nodo = {
+            parada : new Parada(tramo.getDestinoTramo().getidParada(), tramo.getDestinoTramo().getCiudad()),
+            coste : Number(tramo.getPrecioTramo()),
+            padre : nodoActual,
+            hijoIzq : null,
+            hermanoDrch : null
+          };
+          nodoActual.hermanoDrch = hermano;
+          nodoActual = hermano;
+          this.incrementarPeso();
+        }
+      }else{
+        let tienePadre:boolean = false;
+        while(tramo.getOrigenTramo().getCiudad() != nodoActual.padre?.parada?.getCiudad() && nodoActual.padre != null){
+          if(nodoActual.padre != null){
+            nodoActual = nodoActual.padre;
+            tienePadre = true;
+          }
+        }
+        if(tienePadre){
+          let nodo: Nodo = {
+            parada : new Parada(tramo.getDestinoTramo().getidParada(), tramo.getDestinoTramo().getCiudad()),
+            coste : Number(tramo.getPrecioTramo()),
+            padre : nodoActual.padre,
+            hijoIzq : null,
+            hermanoDrch : null
+          }
+          nodoActual.hermanoDrch = nodo;
+          nodoActual = nodo;
+          this.incrementarPeso();
+        }
+      }
+    }
+    
+  }
+}
 
