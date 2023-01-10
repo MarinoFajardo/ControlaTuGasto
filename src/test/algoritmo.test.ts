c22a66399e07106ff267f6d3a8c1dc7feed39ae2
MarinoFajardo <marinofajardo@correo.ugr.es>
2023-01-10 13:22:53 +0100

    closes #41 con las comprobaciones Se han añadido los test para comprobar que el funcionamiento del algoritmo es correcto

--- src/test/algoritmo.test.ts
+++ src/test/algoritmo.test.ts
@@ -1,6 +1,8 @@
 import tramos from "../data/datos.json"
+import { obtenerNodoRaíz, rutaMasBarata } from "../algoritmo";
+import {Arbol, Nodo} from "../arbol";
 import { Tramo } from "../tramo";
-import {Parada} from "../parada"
+import { Parada } from "../parada";
 
 //Función que extrae los elementos del json y los convierte en un array de tramos.
 function tramosData():Tramo[]{
@@ -11,3 +13,31 @@ function tramosData():Tramo[]{
   }
   return salida;
 }
+
+var array:Tramo[] = tramosData();
+
+const numElementos: number = 12;
+const origen: string = "Granada";
+const destino: string = "Otivar";
+const destino2: string = "Jete";
+const valorDeseado: number = 4.30;
+
+const raiz:Nodo = obtenerNodoRaíz(origen,array);
+var arbol:Arbol = new Arbol(raiz);
+
+arbol.generarArbol(array,destino);
+
+console.log(arbol.getPeso());
+
+
+test("La ruta más barata tiene un valor de 4.30",() => {
+  expect(rutaMasBarata(origen,destino,arbol,array)).toBe(valorDeseado);
+})
+
+test("El nodo raíz tiene que ser el origen de la ruta",() => {
+  expect(arbol.getRoot().parada?.getCiudad()).toBe(origen);
+})
+
+test("El valor de la ruta de una parada anterior tiene que ser menor",() => {
+  expect(rutaMasBarata(origen,destino,arbol,array) > rutaMasBarata(origen,destino2,arbol,array)).toBe(true);
+})
