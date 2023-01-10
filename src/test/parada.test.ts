a1ff0008ab3b635f5436a5d926b22cf27c0ad6c2
MarinoFajardo <marinofajardo@correo.ugr.es>
2023-01-10 12:40:17 +0100

    Borrado de test innecesarios

--- src/test/parada.test.ts
+++
@@ -1,8 +0,0 @@
-import {Parada} from "../parada"
-
-const parada1 = new Parada("001","Granada");
-const parada2 = new Parada("002","Almuñécar");
-
-test("Dos paradas de ciudades distintas no pueden tener el mismo ID", () => {
-  expect((parada1.getCiudad()!=parada2.getCiudad())&&(parada1.getidParada()!=parada2.getidParada())).toBe(true);
-});
