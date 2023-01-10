73e6f6e09d5efff6ac28341c2718efbb067744da
MarinoFajardo <marinofajardo@correo.ugr.es>
2023-01-10 12:53:37 +0100

    closes #36 con la extraccion de datos de prueba Se ha creado una función que convierte el json en un vector de tramos.

--- src/test/algoritmo.test.ts
+++ src/test/algoritmo.test.ts
@@ -1,28 +1,13 @@
-import { obtenerNodoRaíz, rutaMasBarata } from "../algoritmo";
-import {Arbol} from "../algoritmo";
+import tramos from "../data/datos.json"
+import { Tramo } from "../tramo";
+import {Parada} from "../parada"
 
-const numElementos: number = 12;
-const origen: string = "Granada";
-const destino: string = "Otivar";
-const destino2: string = "Jete"
-const valorDeseado: number = 4.30;
-
-const raiz = obtenerNodoRaíz(origen);
-let arbol = new Arbol(raiz);
-arbol.aniadirHijos(arbol.getRoot());
-
-test("La ruta más barata tiene un valor de 4.30",() => {
-  expect(rutaMasBarata(origen,destino,arbol)).toBe(valorDeseado);
-})
-
-test("El peso del árbol debe ser igual al número de elementos",() => {
-  expect(arbol.getPeso()).toBe(numElementos);
-})
-
-test("El nodo raíz tiene que ser el origen de la ruta",() => {
-  expect(arbol.getRoot().parada?.getCiudad()).toBe(origen);
-})
-
-test("El valor de la ruta de una parada anterior tiene que ser menor",() => {
-  expect(rutaMasBarata(origen,destino,arbol) > rutaMasBarata(origen,destino2,arbol)).toBe(true);
-})
+//Función que extrae los elementos del json y los convierte en un array de tramos.
+function tramosData():Tramo[]{
+  let salida:Tramo[] = new Array();
+  for(let tramo of tramos){
+    var obj:Tramo = new Tramo(new Parada(tramo.origenTramo.idParada,tramo.origenTramo.ciudad), new Parada(tramo.destinoTramo.idParada,tramo.destinoTramo.ciudad), Number(tramo.precioTramo),Number(tramo.tiempoTramo));
+    salida.push(obj);
+  }
+  return salida;
+}
