4bbfb5dc012b1706fe8c158fdf9e1f118780ae2a
MarinoFajardo <marinofajardo@correo.ugr.es>
2023-01-24 16:58:21 +0100

    closes #55 añadiendo test correctos. Se han eliminado los test que testeaban resultados y se han añadido nuevos que testean comportamiento.

--- src/test/algoritmo.test.ts
+++ src/test/algoritmo.test.ts
@@ -1,4 +1,4 @@
-import tramos from "./data/datos.json"
+import  tramosjson from "./data/datos.json"
 import { obtenerNodoRaíz, rutaMasBarata } from "../algoritmo";
 import {Arbol, Nodo} from "../arbol";
 import { Tramo } from "../tramo";
@@ -10,14 +10,14 @@ import 'mocha';
 //Función que extrae los elementos del json y los convierte en un array de tramos.
 function tramosData():Tramo[]{
   let salida:Tramo[] = new Array();
-  for(let tramo of tramos){
+  for(let tramo of tramosjson){
     var obj:Tramo = new Tramo(new Parada(tramo.origenTramo.idParada,tramo.origenTramo.ciudad), new Parada(tramo.destinoTramo.idParada,tramo.destinoTramo.ciudad), Number(tramo.precioTramo),Number(tramo.tiempoTramo));
     salida.push(obj);
   }
   return salida;
 }
 
-var array:Tramo[] = tramosData();
+var tramos:Tramo[] = tramosData();
 
 const pesoArbol1:number = 13;
 const pesoArbol2:number = 5;
@@ -26,42 +26,18 @@ const origen: string = "Granada";
 const origen2: string = "Salobreña";
 const destino: string = "Otivar";
 const destino2: string = "Jete";
-const valorDeseado: number = 4.2;
-const valorDeseado2: number = 2.1;
+const destino3: string = "Almuñecar";
 
-const raiz:Nodo = obtenerNodoRaíz(origen,array);
-const raiz2:Nodo = obtenerNodoRaíz(origen2,array);
-
-const arbol:Arbol = new Arbol(raiz,destino,array);
-const arbol2:Arbol = new Arbol(raiz2,destino2,array);
-
-describe("La ruta más barata entre Granada y Otívar tiene un valor de 4.20", () => {
-  it("Debe devolver 4.20", () => {
-    expect(rutaMasBarata(origen,destino,arbol,array)).to.equal(valorDeseado);
+describe("La entre Granada y Otívar tiene que ser más cara que la ruta entre Granada y Almuñécar", () => {
+  it("Debe devolver true", () => {
+    expect(rutaMasBarata(origen,destino,tramos)).to.greaterThan(rutaMasBarata(origen,destino3,tramos));
   });
 });
 
-describe("La ruta más barata entre Salobreña y Jete tiene un valor de 2.10", () => {
-  it("Debe devolver 2.1", () => {
-    expect(rutaMasBarata(origen2,destino2,arbol2,array)).to.equal(valorDeseado2);
+describe("La ruta entre Salobreña y Jete tiene que ser más barata que la ruta entre Salobreña y Otívar", () => {
+  it("Debe devolver true", () => {
+    expect(rutaMasBarata(origen2,destino2,tramos)).to.below(rutaMasBarata(origen2,destino,tramos));
   });
 });
 
-describe("El nodo raíz tiene que ser el origen de la ruta", () => {
-  it("Debe devolver Granada", () => {
-    expect(arbol.getRoot().parada?.getCiudad()).to.equal(origen);
-  });
-});
-
-describe("El peso del árbol generado para calcular las rutas entre Granada y Otívar tiene que ser 13", () => {
-  it("Debe devolver 13", () => {
-    expect(arbol.getPeso()).to.equal(pesoArbol1);
-  });
-});
-
-describe("El peso del árbol generado para calcular las rutas entre Salobreña y Jete tiene que ser 5", () => {
-  it("Debe devolver 5", () => {
-    expect(arbol2.getPeso()).to.equal(pesoArbol2);;
-  });
-});
 
